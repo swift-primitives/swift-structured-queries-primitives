e7c5d89db4e9bbfd73ac88054440141c89c06b86
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-06-01 23:30:52 +0200

    Relax StructuredQueries Tagged-trait phantom Tag to ~Copyable & ~Escapable (cascade, Tier-1)
    5 Tagged conformances (_OptionalPromotable/QueryBindable/QueryDecodable/QueryExpression/
    QueryRepresentable) widened. §1.3 v1.2.0. Builds green 6.3.

--- Sources/Structured Queries Primitives/Traits/Tagged.swift
+++ Sources/Structured Queries Primitives/Traits/Tagged.swift
@@ -1,26 +1,26 @@
 public import Tagged_Primitives
 
-extension Tagged: _OptionalPromotable where Tag: ~Copyable, Underlying: _OptionalPromotable {}
+extension Tagged: _OptionalPromotable where Tag: ~Copyable & ~Escapable, Underlying: _OptionalPromotable {}
 
-extension Tagged: QueryBindable where Tag: ~Copyable, Underlying: QueryBindable {
+extension Tagged: QueryBindable where Tag: ~Copyable & ~Escapable, Underlying: QueryBindable {
     public var queryBinding: QueryBinding {
         underlying.queryBinding
     }
 }
 
-extension Tagged: QueryDecodable where Tag: ~Copyable, Underlying: QueryDecodable {
+extension Tagged: QueryDecodable where Tag: ~Copyable & ~Escapable, Underlying: QueryDecodable {
     public init(decoder: inout some QueryDecoder) throws {
         self.init(_unchecked: try Underlying(decoder: &decoder))
     }
 }
 
-extension Tagged: QueryExpression where Tag: ~Copyable, Underlying: QueryExpression {
+extension Tagged: QueryExpression where Tag: ~Copyable & ~Escapable, Underlying: QueryExpression {
     public var queryFragment: QueryFragment {
         underlying.queryFragment
     }
 }
 
-extension Tagged: QueryRepresentable where Tag: ~Copyable, Underlying: QueryRepresentable {
+extension Tagged: QueryRepresentable where Tag: ~Copyable & ~Escapable, Underlying: QueryRepresentable {
     public typealias QueryOutput = Tagged<Tag, Underlying.QueryOutput>
 
     public var queryOutput: QueryOutput {
