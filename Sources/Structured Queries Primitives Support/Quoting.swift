1d01ea71c9a8867448399a399b4ade244e2f5990
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-07-07 15:04:28 +0200

    docs: author public-surface summaries (Structured Queries Primitives Support)

--- Sources/Structured Queries Primitives Support/Quoting.swift
+++ Sources/Structured Queries Primitives Support/Quoting.swift
@@ -1,11 +1,13 @@
 import Foundation
 
+/// A delimiter used to quote SQL identifiers or text literals.
 public enum QuoteDelimiter: String {
     case identifier = "\""
     case text = "'"
 }
 
 extension StringProtocol {
+    /// Returns this string quoted with the given delimiter, escaping embedded delimiters.
     public func quoted(_ delimiter: QuoteDelimiter = .identifier) -> String {
         let delimiter = delimiter.rawValue
         return delimiter + replacingOccurrences(of: delimiter, with: delimiter + delimiter)
